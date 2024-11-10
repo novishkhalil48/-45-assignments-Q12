cad9ab44fcc1713cb116d4876bc916242c434875
Novish Khalil <novishkhalil2@hotmail.com>
2024-11-11 00:18:43 +0500

    Add files via upload

---
+++ Q12.js
@@ -0,0 +1,6 @@
+"use strict";
+//12.Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them. The text of each message should be the same, but each message should be personalized with the person’s name.
+let prNam = ["Sara", "Amna", "Rida"];
+console.log("Hey how are you doing", pName[0]);
+console.log("Hey how are you doing", pName[1]);
+console.log("Hey how are you doing", pName[2]);
---
+++ Q12.ts
@@ -0,0 +1,5 @@
+//12.Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them. The text of each message should be the same, but each message should be personalized with the person’s name.
+let prNam = ["Sara", "Amna", "Rida"];
+console.log("Hey how are you doing", prNam[0]);
+console.log("Hey how are you doing", prNam[1]);
+console.log("Hey how are you doing", prNam[2]);
